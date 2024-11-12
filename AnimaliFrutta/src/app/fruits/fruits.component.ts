7c2556866bc497914f0048355fe0683bc193039a
Giulia De Angeli <deangeli.giulia@iisgalvanimi.edu.it>
2024-11-12 09:53:05 +0000

    parte 1

--- AnimaliFrutta/src/app/fruits/fruits.component.ts
+++ AnimaliFrutta/src/app/fruits/fruits.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./fruits.component.css']
 })
 export class FruitsComponent {
-
+  fruits = [
+    { name: 'tufah', emoji: '🍎' },
+    { name: 'banana', emoji: '🍌' },
+    { name: '3jaj', emoji: '🍍' },
+    { name: 'limon', emoji: '🍋' },
+    { name: '7ar', emoji: '🍉' },
+    { name: 'frah', emoji: '🍓' },
+    { name: 'kaki', emoji: '🍊' },
+    { name: '9hesi', emoji: '🍑' },
+    { name: '7an9al', emoji: '🍈' },
+    { name: 'ber9oq', emoji: '🍒' },
+    { name: 'kiwi', emoji: '🥝' },
+    { name: 'toffah', emoji: '🍏' },
+    { name: '3nabi', emoji: '🍇' },
+    { name: 'tamar', emoji: '🌰' },
+    { name: 'fig', emoji: '🍈' },
+    { name: 'bal3', emoji: '🍍' },
+    { name: 'n5awer', emoji: '🍒' },
+    { name: 'zibda', emoji: '🥥' },
+    { name: 'doum', emoji: '🍉' },
+    { name: 'j4ss', emoji: '🍑' },
+    { name: 'sefraya', emoji: '🍋' },
+    { name: 'mango', emoji: '🥭' },
+    { name: 'lem', emoji: '🍋' },
+    { name: 'tamarind', emoji: '🌶️' },
+    { name: 'ananas', emoji: '🍍' },
+    { name: 'jambri', emoji: '🍏' },
+    { name: 'tini', emoji: '🍓' },
+    { name: 'beres', emoji: '🍒' },
+    { name: '3thm', emoji: '🍊' },
+    { name: 'pistachio', emoji: '🌰' },
+    { name: 'sitrus', emoji: '🍊' }
+  ];
+  
 }
