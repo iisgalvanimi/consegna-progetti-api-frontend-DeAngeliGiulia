3aec845fd496361f626edd8fcffbc8e9accbd60b
Giulia De Angeli <deangeli.giulia@iisgalvanimi.edu.it>
2024-11-12 10:01:28 +0000

    generic funziona

---
+++ AnimaliFrutta/src/app/generic/generic.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+@Component({
+  selector: 'app-generic',
+  templateUrl: './generic.component.html',
+  styleUrls: ['./generic.component.css']
+})
+export class GenericComponent {
+  constructor(private route: ActivatedRoute) {
+    this.route.paramMap.subscribe(this.getRouterParam);
+  }
+  getRouterParam = (params: ParamMap) =>
+    {
+      let uri_param = params.get('id'); //Ottengo l'id dalla ParamMap
+      console.log (uri_param); //Stampo su console
+      //this.service.getTrack()
+    }
+
+}
