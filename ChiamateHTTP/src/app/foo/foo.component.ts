6a13a23f1331e50a261b4df12b6c94e119df70f3
Giulia De Angeli <deangeli.giulia@iisgalvanimi.edu.it>
2024-11-15 11:45:36 +0000

    15.2

--- ChiamateHTTP/src/app/foo/foo.component.ts
+++ ChiamateHTTP/src/app/foo/foo.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {Foo} from './foo.model';
 
 @Component({
  selector: 'app-foo',
  templateUrl: './foo.component.html',
- styleUrls: ['./foo.component.css']
+ styleUrls: ['./foo.component.css'],
+
 })
-export class FooComponent {
+export class FooComponent implements OnInit{
    data!: Object; //Il ‘!’ serve a creare variabili non inizializzate
    loading: boolean=false;
    o! :Observable<Object>;
+   fooData! : Foo[];
+   oFoo! : Observable<Foo[]>
    constructor(public http: HttpClient) {}
    makeRequest(): void {
      console.log("here");
@@ -48,5 +52,11 @@ export class FooComponent {
             this.loading = false;
           });
       }
-     
+      makeTypedRequest() : void {//oFoo : Observable<Foo[]>; va dichiarato tra gli attributi della classe
+        this.oFoo = this.http.get<Foo[]>('https://jsonplaceholder.typicode.com/posts');
+        this.oFoo.subscribe(data => {this.fooData = data;});
+      }
+      ngOnInit() {
+      }
+
 }
